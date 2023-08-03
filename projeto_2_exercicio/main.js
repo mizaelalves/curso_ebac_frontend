70934acec1e273b18576a41101b7db3a5d72e8fa
mizaelalves <mizaelbna@hotmail.com>
2023-08-03 00:24:26 -0300

    first commit

---
+++ projeto_2_exercicio/main.js
@@ -0,0 +1,55 @@
+const form = document.getElementById('form-atividade')
+
+let nome = []
+let telefone = []
+
+let linhas = ''
+
+
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
+  adicionarLinha();
+  atualizarTabela()
+
+})
+
+function adicionarLinha() {
+
+  const inputNome = document.getElementById('nome-contato')
+  const inputTelefone = document.getElementById('telefone-contato')
+  let id
+
+  if (telefone.includes(parseFloat(inputTelefone.value))) {
+    alert("Contato ja inserido, por favor adicione outro contato!")
+  } else {
+    nome.push(inputNome.value)
+    telefone.push(parseFloat(inputTelefone.value))
+    id = telefone.length
+
+    let linha = `<tr>`
+    linha += `<td>${id}</td>`
+    linha += `<td>${inputNome.value}</td>`
+    linha += `<td>${inputTelefone.value}</td>`
+
+    linha += `</tr>`
+
+    linhas += linha
+
+
+  }
+
+
+
+  inputNome.value = ''
+  inputTelefone.value = ''
+
+
+}
+
+function atualizarTabela() {
+  const corpoTabela = document.querySelector('tbody')
+  corpoTabela.innerHTML = linhas
+
+}
+
